54f1031a7fd2ec7cf3be26514dc09bec1da32479
valera996 <valera.muhin.96@mail.ru>
2026-01-13 21:08:18 +0300

    Домашняя работа 3

---
+++ src/DZ3/Library/LibraryTest.java
@@ -0,0 +1,15 @@
+package DZ3.Library;
+
+public class LibraryTest {
+    public static void main(String[] args) {
+
+        Library lab1 = new Library();
+
+        lab1.setBookTitle("ыекрук");
+        lab1.author = "укицук";
+
+
+    }
+
+
+}
