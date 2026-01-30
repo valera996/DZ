fc93378118a61bf33dfed8256257a789430f17f8
valera996 <valera.muhin.96@mail.ru>
2026-01-30 17:32:03 +0300

    Исправления

--- src/DZ7/ExceptionTasks/FourthTask.java
+++ src/DZ7/ExceptionTasks/FourthTask.java
@@ -4,23 +4,47 @@ import java.util.regex.Pattern;
 
 public class FourthTask {
 
-    public static void checkEmail(String email) throws MyRuntimeException {
-        try {
-           String pattern = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
-           if(!Pattern.matches(pattern, email)){
-               throw new MyRuntimeException();
-           }else System.out.println("email валидный");
-        }catch (MyRuntimeException e){
-            System.out.println(e.getMessage());
-        }
-    }
+//    public static void checkEmail(String email) throws MyRuntimeException {
+//        try {
+//           String pattern = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+//           if(!Pattern.matches(pattern, email)){
+//               throw new MyRuntimeException();
+//           }else System.out.println("email валидный");
+//        }catch (MyRuntimeException e){
+//            System.out.println(e.getMessage());
+//        }
+//    }
+
+    //Исправленный вариант
+public static void checkEmail(String email) throws MyRuntimeException {
+        String pattern = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+        if(!Pattern.matches(pattern, email)){
+            throw new MyRuntimeException();
+        }else System.out.println("email валидный");
+}
+
 
     public static void main(String[] args) {
 
-        //Валидный email
-        checkEmail("sfbwsfb@mail.ru");
+//        //Валидный email
+//        checkEmail("sfbwsfb@mail.ru");
+//
+//        //Не валидный email, отрабатывает исколючение
+//        checkEmail("цкцкууа");
 
+
+        //Исправленный вариант
+        //Валидный email
+       try {
+           checkEmail("sfbwsfb@mail.ru");
+       }catch(MyRuntimeException e){
+           System.out.println(e.getMessage());
+       }
         //Не валидный email, отрабатывает исколючение
-        checkEmail("цкцкууа");
+       try {
+           checkEmail("цкцкууа");
+       }catch (MyRuntimeException e){
+           System.out.println(e.getMessage());
+       }
     }
 }
